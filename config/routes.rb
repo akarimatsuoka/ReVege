f767f7577ad477c1e0dc99996be2b6a372d08745
akarimatsuoka <114091227+achan-m@users.noreply.github.com>
2022-12-05 07:20:00 +0000

    start

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,26 @@
 Rails.application.routes.draw do
-  # 顧客用
-# URL /customers/sign_in ...
-devise_for :customers, skip: [:passwords],controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
-# 顧客用
-scope module: :public do
-	root to:"homes#top"
-  get "about"=>"homes#about",as:"about"
-end
+  
+      # 顧客用
+    # URL /customers/sign_in ...
+    devise_for :customers, skip: [:passwords],controllers: {
+      registrations: "public/registrations",
+      sessions: 'public/sessions'
+    }
+    
+    scope module: :public do
+    	root to:"homes#top"
+      get "about"=>"homes#about",as:"about"
+    end
+    
+    
+    # 管理者用
+    # URL /admin/sign_in ...
+    devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+      sessions: "admin/sessions"
+    }
+    
+    scope module: :admin do
+      resources :genres
+    end
 
 end
