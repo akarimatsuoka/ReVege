d2578614027822cb24a2eca23aed217d0efc7889
akarimatsuoka <114091227+achan-m@users.noreply.github.com>
2022-12-05 08:47:54 +0000

    start

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,10 +1,20 @@
 class Admin::GenresController < ApplicationController
-  def new
-  end
 
   def index
+    @genre=Genre.new
+    @genres=Genre.all
+  end
+
+  def create
+    @genre=Genre.new(genre_params)
+    @genre.save
+    redirect_to genres_path
   end
 
   def edit
+  end
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
