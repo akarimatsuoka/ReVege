12880a658c226ec2659604eb32f62226de994ad3
akarimatsuoka <114091227+achan-m@users.noreply.github.com>
2022-12-06 13:17:45 +0000

    feature_shops

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -3,9 +3,26 @@ class Admin::ShopsController < ApplicationController
     @shop=Shop.new
   end
 
+  def create
+    @shop=Shop.new(shop_params)
+    if @shop.save
+      redirect_to admin_shop_path(@shop.id)
+    else
+      render :new
+    end
+  end
+
+   def index
+     @shops=Shop.all
+   end
+
   def edit
   end
 
-  def index
+  private
+
+  def shop_params
+    params.require(:shop).permit(:image, :name, :introduction, :postal_code, :prefecture, :address, :phone_number)
   end
+
 end
