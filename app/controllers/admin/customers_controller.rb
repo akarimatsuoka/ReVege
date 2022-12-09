04b0ca2b79f1f86b9962052016d8a2cfa03089e1
akarimatsuoka <114091227+achan-m@users.noreply.github.com>
2022-12-09 06:42:28 +0000

    feature_genre

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-     @customers = Customer.all.order(created_at: "DESC")
+     @customers = Customer.all.order(created_at: "DESC") #created_at: "DESC"→最新のものから順に並べる
   end
 
   def show
@@ -10,7 +10,7 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
@@ -19,11 +19,11 @@ class Admin::CustomersController < ApplicationController
      render "edit"
     end
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,:email, :postal_code, :address, :phone_number, :is_deleted)
   end
-  
+
 end
