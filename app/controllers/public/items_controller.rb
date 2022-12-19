bdd32e33bbfae54fb02fa57386502af541b0a135
akarimatsuoka <114091227+achan-m@users.noreply.github.com>
2022-12-19 04:44:58 +0000

    pagenation

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -14,14 +14,14 @@ class Public::ItemsController < ApplicationController
       @search = @genre.items.ransack(params[:q])
     end
     # 検索結果
-    @items = @search.result.where(status: true) #@searchで出力したitem（statusがtrueのもののみ）が一覧で出る
+    @items = @search.result.where(status: true).page(params[:page]) #@searchで出力したitem（statusがtrueのもののみ）が一覧で出る
     @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
-    
+
     if params[:genre_id].present? #ジャンル検索のみ実行されてる場合
       @genre = Genre.find(params[:genre_id])
     elsif params[:q] && params[:q][:genre_id].present? #検索ボタンが押されてて、かつジャンルがある場合
