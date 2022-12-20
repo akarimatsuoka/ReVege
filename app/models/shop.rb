b3626e0221b62785178b7c6e37d20c386c4fb216
akarimatsuoka <114091227+achan-m@users.noreply.github.com>
2022-12-20 04:22:42 +0000

    feature

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -13,6 +13,14 @@ class Shop < ApplicationRecord
     bookmarks.where(customer_id: customer).exists?
   end
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :postal_code, presence: true
+  validates :prefecture, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+
 
 #12,15,16,17行目はenumを使用した時の記述（jp_prefectureを使用してればstring型のprefectureカラムを用意さえすればenum使わなくていい）
   # enum prefecture: { hokkaido: 0, aomori: 1, akita: 2, iwate: 3, yamagata: 4, miyagi: 5, fukushima: 6, niigata: 7, gunma: 8, tochigi: 9, ibaraki: 10, saitama: 11, yamanashi: 12, chiba: 13, tokyo: 14, kanagawa: 15, nagano: 16, shizuoka: 17, toyama: 18, ishikawa: 19, gifu: 20, fukui: 21, aichi: 22, shiga: 23, mie: 24, kyoto: 25, osaka: 26, nara: 27, hyogo: 28, wakayama: 29, tottori: 30, okayama: 31, shimane: 32, hiroshima: 33, yamaguchi: 34, kagawa: 35, tokushima: 36, ehime: 37, kochi: 38, fukuoka: 39, saga: 40, nagasaki: 41, oita: 42, kumamoto: 43, miyazaki: 44, kagoshima: 45, okinawa: 46 }
